2e763e8c6398b10158b01635e7988be536ae11e4
Iamsdt <shudiptotrafder@gmail.com>
2017-06-25 14:33:12 +0600

    Removed find view by id

--- app/src/main/java/com/iamsdt/movienewsv3/DetailsActivity.kt
+++ app/src/main/java/com/iamsdt/movienewsv3/DetailsActivity.kt
@@ -1,10 +1,9 @@
 package com.iamsdt.movienewsv3
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -12,11 +11,8 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
--- app/src/main/java/com/iamsdt/movienewsv3/MainActivity.kt
+++ app/src/main/java/com/iamsdt/movienewsv3/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.iamsdt.movienewsv3
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 
@@ -16,14 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
+        main_fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
